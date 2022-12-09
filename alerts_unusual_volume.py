6ddeba8cb2f1af2080e66af3c0d11778d8f16558
Anish Nagariya <anishnagariya07@gmail.com>
2022-12-09 10:30:42 -0500

    Update alerts_unusual_volume.py

--- alerts_unusual_volume.py
+++ alerts_unusual_volume.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     @sched.scheduled_job(CronTrigger(minute="*/5"))
     async def unusual_volume():
         # checking if timings are met for regular hours
-        if 10 <= datetime.now().hour < 16 or datetime.now().hour == 9 and datetime.now().minute >= 30:
+        if 15 <= datetime.now().hour < 21 or datetime.now().hour == 14 and datetime.now().minute >= 30:
             start = time.time()
             embeds_ = []
             # multiprocessing the function detect_vol for all tickers
